d38ead309eb2cdce2b0edee75192b384b60a6c22
alberto <alberto.astudillo@wearestrings.com>
2017-11-23 13:53:07 +0100

    Changed document.rb toledocensuscaller

---
+++ app/models/custom/verification/management/document.rb
@@ -0,0 +1,47 @@
+# TOL-14 Overwritten document with custom census
+class Verification::Management::Document
+  include ActiveModel::Model
+  include ActiveModel::Dates
+
+  attr_accessor :document_type
+  attr_accessor :document_number
+
+  validates :document_type, :document_number, presence: true
+
+  delegate :username, :email, to: :user, allow_nil: true
+
+  def user
+    @user = User.active.by_document(document_type, document_number).first
+  end
+
+  def user?
+    user.present?
+  end
+
+  def in_census?
+    response = ToledoCensusCaller.new.call(document_type, document_number)
+    response.valid? && valid_age?(response)
+  end
+
+  def valid_age?(response)
+    if under_age?(response)
+      errors.add(:age, true)
+      false
+    else
+      true
+    end
+  end
+
+  def under_age?(response)
+    response.date_of_birth.blank? || Age.in_years(response.date_of_birth) < User.minimum_required_age
+  end
+
+  def verified?
+    user? && user.level_three_verified?
+  end
+
+  def verify
+    user.update(verified_at: Time.current) if user?
+  end
+
+end
